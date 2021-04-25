e6baf18f9acd3b6f4d95f59aef708f284eddb711
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-04-25 19:35:35 -0300

    Handlebars

---
+++ routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const product = require('../controllers/product')
+
+router.get("/vista", (req, res) => {
+    const products = product.get()
+    res.render("products", {Productos: products});
+})
+
+router.post("/", (req, res) => {
+    const data = req.body
+    if (product.add(data)) {
+        if(data.form === '1') return res.redirect("http://localhost:8080")
+        res.status(201).json(data)
+    }
+    res.status(400).send()
+})
+
+router.put("/:id", (req, res) => {
+    const data = req.body
+    const { id } = req.params
+    if (product.update(id, data)) {
+        res.status(201).json(data)
+    }
+    res.status(400).send()
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+    const currentProduct = product.getById(id)
+    if (currentProduct) {
+        return res.json(currentProduct)
+    }
+    res.status(404).json({ error: "producto no encontrado" })
+})
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params
+    product.remove(id)
+    res.send()
+})
+
+module.exports = router
---
+++ src/main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const productsRoutes = require('../routes/products')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', exphbs({
+    extname: "hbs",
+}));
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) =>{
+    res.render("home")
+})
+
+app.use('/productos', productsRoutes)
+
+app.listen(8080)
